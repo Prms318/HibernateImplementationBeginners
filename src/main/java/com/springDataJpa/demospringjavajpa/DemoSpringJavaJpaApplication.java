2857fa15b413e16973730fcc0a5fc59ce86066be
Pramesh Baral <prms.brl@gmail.com>
2024-04-12 12:13:38 -0500

    Project created

---
+++ src/main/java/com/springDataJpa/demospringjavajpa/DemoSpringJavaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springDataJpa.demospringjavajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJavaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringJavaJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springDataJpa/demospringjavajpa/DemoSpringJavaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDataJpa.demospringjavajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringJavaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
